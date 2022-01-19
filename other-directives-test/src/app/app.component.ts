55d389a6b4ee1f6c0122e308ef03790722ae128f
Aidan Killeen <aidan.killeen@gmail.com>
2022-01-19 13:40:14 +0000

    added ngSwitch example

--- other-directives-test/src/app/app.component.ts
+++ other-directives-test/src/app/app.component.ts
@@ -18,13 +18,12 @@ import { User } from './user.model';
       </thead>
       <tbody>
         <tr *ngFor="let user of users"
+          [ngSwitch]="user.country" 
           [ngClass]="{active:user.active, inactive:!user.active}"
         >
           <td>{{ user.id }}</td>
           <td [ngStyle]="{'text-decoration': user.active ? 'underline' : 'line-through'}">{{ user.name }}</td>
           <td>{{ user.email }}</td>
-          
-          
           <td *ngIf="user.active"><strong>Active</strong></td>
           <td *ngIf="!user.active">***Inactive User***</td>
 
@@ -33,7 +32,14 @@ import { User } from './user.model';
             <td>INACTIVE</td>
           </ng-template>
 
-          <td>{{ user.country }}</td>
+          <td *ngSwitchCase = "'IE'"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Flag_of_Ireland.svg/255px-Flag_of_Ireland.svg.png" height="25"> - {{ user.country }}</td>
+          <td *ngSwitchCase = "'DE'"><img src="https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/255px-Flag_of_Germany.svg.png" height="20">{{ user.country }}</td>
+          <td *ngSwitchCase = "'FR'"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Flag_of_France_%281794%E2%80%931815%2C_1830%E2%80%931958%29.svg/250px-Flag_of_France_%281794%E2%80%931815%2C_1830%E2%80%931958%29.svg.png" height="20"> {{ user.country }}</td>
+          <td *ngSwitchDefault>SOMEWHERE ELSE - {{ user.country }}</td>
+
+
+
+
         </tr>
       </tbody>
     </table>
@@ -57,7 +63,7 @@ export class AppComponent {
     new User(3, "Carol", "alice@gmail.com", true, "DE"),
     new User(4, "Dan", "alice@gmail.com", false, "IE"),
     new User(5, "Eve", "alice@gmail.com", true, "DE"),
-    new User(6, "Fred", "fred@gmail.com", true, "FR")
+    new User(6, "Fred", "fred@gmail.com", true, "USA")
 
   ]
 }
